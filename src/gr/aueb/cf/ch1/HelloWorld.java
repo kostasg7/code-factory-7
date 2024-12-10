66c49768901b28d79a92a520a26fe3cbc2232744
K. Papadopoulos <kostashope@hotmail.com>
2024-12-10 20:05:04 +0200

    initial commit

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,21 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * Prints Hello World in
+ * standard output (console).
+ *
+ * Documentation comments
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        //implementation comments
+
+        /*
+        implementation comments
+         */
+
+        System.out.println("Hello World!"); //here too
+    }
+}

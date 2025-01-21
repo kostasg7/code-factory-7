576367e27cdfcd0141d69240e33419f91f655998
K. Papadopoulos <kostashope@hotmail.com>
2025-01-21 16:39:19 +0200

    update

---
+++ src/gr/aueb/cf/ch16/sceletal/IRectangle.java
@@ -0,0 +1,7 @@
+package gr.aueb.cf.ch16.sceletal;
+
+public interface IRectangle {
+    double getArea();
+    double getPerimeter();
+    boolean isSquare();
+}
---
+++ src/gr/aueb/cf/ch16/sceletal/Square.java
@@ -0,0 +1,5 @@
+package gr.aueb.cf.ch16.sceletal;
+
+public class Square extends AbstractRectangle{
+
+}

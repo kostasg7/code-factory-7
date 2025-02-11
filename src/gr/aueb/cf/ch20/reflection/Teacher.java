10d0bb51e20ac01228c6f7d48e114d22fa07cee4
K. Papadopoulos <kostashope@hotmail.com>
2025-02-11 21:02:59 +0200

    feat: enums, lambdas, reflection and assertion examples added

---
+++ src/gr/aueb/cf/ch20/reflection/Teacher.java
@@ -0,0 +1,52 @@
+package gr.aueb.cf.ch20.reflection;
+
+public class Teacher {
+    private int id;
+    private String firstname;
+    private String lastname;
+
+    public Teacher() {
+    }
+
+    public Teacher(int id)  {
+        this.id = id;
+    }
+
+    public Teacher(int id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+
+    public void saySomething(String message) {
+        System.out.println(message);
+    }
+}

52355e64a5dfe66ab14438accf86e800f582b478
K. Papadopoulos <kostashope@hotmail.com>
2025-03-18 20:26:23 +0200

    feat: echo client and iterative server created

---
+++ src/gr/aueb/cf/ch26/EchoServerApp.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.ch26;
+
+import gr.aueb.cf.ch26.EchoServer;
+
+public class EchoServerApp {
+
+    public static void main(String[] args) {
+        EchoServer echoServer = new EchoServer();
+        echoServer.start();
+    }
+}
